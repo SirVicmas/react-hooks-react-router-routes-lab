4142a440c97bdaabc8bd9c9aa575b72a1ab71f4c
Masibo <victor.wafula@student.moringaschool.com>
2023-08-25 12:05:07 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+ 
   </Router>,
   document.getElementById("root")
 );
